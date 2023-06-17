bb4c91f81106e0b2265e5e86cbc355268092dc4f
bkrishna1114 <bkrishna1114@gmail.com>
2023-06-18 02:44:40 +0530

    onchange & On submit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{Component,useEffect,useState} from "react";
 import Display from "./display";
 import './App.css';
+import axios from "axios";
 
 // const App  = () => {
 //   return(
@@ -87,26 +88,98 @@ import './App.css';
 
 
 // using UseEffect{} in from react hook..
-const App  = () => {
-  const [count,setCount] = useState(0)
-  useEffect(
-    () => console.log(count,[count])
-  )
+// const App  = () => {
+//   const [count,setCount] = useState(0)
+//   useEffect(
+//     () => console.log(count,[count])
+//   )
+//   return(
+//     <div>
+//       <center>
+//         <h1>I have {count} Iphones</h1>
+//         <button className="submit"
+//           onClick={
+//             () => setCount(count+1)
+//           }
+//           >
+//           click Here
+//         </button>
+//       </center>
+//     </div>
+//   )
+// }
+
+// OnChange Event handeler..
+// const App = () =>{
+//   const [user,setUser] = useState("");
+//   const handeler = e => {
+//     setUser(e.target.value)
+//   }
+//   return(
+//     <div>
+//       <center>
+//           <h1>Welcome to react {user}</h1>
+//           <input 
+//               type="text" 
+//               placeholder="username" 
+//               value={user} 
+//               name="user"
+//               onChange={handeler}
+//           />
+//           <br/>
+//           <h1>{user}</h1>
+//       </center>
+//     </div>
+//   )
+// }
+
+//Onsubmit Handler...
+const App = ( )=> {
+	const [data,setData] = useState({
+		username : "",
+		password : "",
+	})
+	const {username,password} = data;
+	//onchange.. usename , password..
+	const onChange = e=>{
+		setData({...data,[e.target.name]:[e.target.value]})
+	}
+
+	//Onsubmit Handler..
+	const submitHandler = e =>{
+		e.preventDefault();
+		console.log(data);
+		//to send data to the backend..
+		// axios.post('url',data);
+	}
   return(
-    <div>
-      <center>
-        <h1>I have {count} Iphones</h1>
-        <button className="submit"
-          onClick={
-            () => setCount(count+1)
-          }
-          >
-          click Here
-        </button>
-      </center>
-    </div>
+	<center>
+		<form onSubmit={submitHandler}>
+		<input
+			type="text"
+			name="username"
+			value={username}
+			onChange={onChange}
+			placeholder="user name"
+		/>
+		<br/>
+		<input
+			type="password"
+			name="password"
+			value={password}
+			onChange={onChange}
+			placeholder="password"
+		/>
+		<br/>
+		<input 
+			type="submit"
+			name="submit"
+		/>
+		</form>
+	</center>
   )
 }
+
 
 export default App;
 
