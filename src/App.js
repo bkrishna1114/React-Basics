e8a68e834985bc2cfacdd5ff310567b4cc68cddc
bkrishna1114 <bkrishna1114@gmail.com>
2023-06-17 15:16:59 +0530

    use state, useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{Component,useState} from "react";
+import React,{Component,useEffect,useState} from "react";
 import Display from "./display";
 import './App.css';
 
@@ -39,26 +39,75 @@ import './App.css';
 
 // export default App;
 
-// state and props...
-export default class App extends Component {
-  state = {
-    name : "Balu",
-  }
-  render(){
-    return(
-      <div className='container'>
-        <center>
-          <h1>{this.state.name} Hello World !</h1>
-          <Display name={this.state.name} />
-          <button className="submit"
-              onClick={
-                ()=>console.log("clicked")
-              }
+// state and props...and use state
+// export default class App extends Component {
+//   state = {
+//     name : "Balu",
+//   }
+//   render(){
+//     return(
+//       <div className='container'>
+//         <center>
+//           <h1>{this.state.name} Hello World !</h1>
+//           <Display name={this.state.name} />
+//           <button className="submit"
+//               onClick={
+//                 ()=>console.log("clicked")
+//               }
               
-          >Submit</button>
-        </center>
-      </div>
-    )
-  }
+//           >Submit</button>
+//         </center>
+//       </div>
+//     )
+//   }
+// }
+
+///onclick use state...
+// const App  = () => {
+//   const [count,setCount] = useState(0)
+
+//   return(
+//     <div>
+//       <center>
+//         <h1>{count}</h1>
+//         <button className="submit"
+//           onClick={
+//             () => setCount(count+1)
+//           }
+//           >
+//           click Here
+//         </button>
+//       </center>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
+
+// using UseEffect{} in from react hook..
+const App  = () => {
+  const [count,setCount] = useState(0)
+  useEffect(
+    () => console.log(count,[count])
+  )
+  return(
+    <div>
+      <center>
+        <h1>I have {count} Iphones</h1>
+        <button className="submit"
+          onClick={
+            () => setCount(count+1)
+          }
+          >
+          click Here
+        </button>
+      </center>
+    </div>
+  )
 }
+
+export default App;
+
 
